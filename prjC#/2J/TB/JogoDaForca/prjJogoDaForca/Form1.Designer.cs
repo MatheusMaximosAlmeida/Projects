e61f2f9e3a27175c2694cfe5218cb4b5228f6160
Matheus Maximos Almeida <88410053+MatheusMaximosAlmeida@users.noreply.github.com>
2022-03-10 22:40:35 -0300

    modified prjForca

--- prjC#/2J/TB/JogoDaForca/prjJogoDaForca/Form1.Designer.cs
+++ prjC#/2J/TB/JogoDaForca/prjJogoDaForca/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtLetra = new System.Windows.Forms.TextBox();
             this.btnJogar = new System.Windows.Forms.Button();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnPalavra = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -46,18 +46,18 @@
             this.panel1.Size = new System.Drawing.Size(190, 174);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // btnJogar
             // 
-            this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 48);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "DIGITE UMA LETRA";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnJogar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnJogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJogar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogar.ForeColor = System.Drawing.Color.White;
+            this.btnJogar.Location = new System.Drawing.Point(3, 114);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(184, 42);
+            this.btnJogar.TabIndex = 2;
+            this.btnJogar.Text = "JOGAR";
+            this.btnJogar.UseVisualStyleBackColor = false;
             // 
             // txtLetra
             // 
@@ -71,18 +71,18 @@
             this.txtLetra.Text = "A";
             this.txtLetra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // btnJogar
+            // label1
             // 
-            this.btnJogar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnJogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnJogar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnJogar.ForeColor = System.Drawing.Color.White;
-            this.btnJogar.Location = new System.Drawing.Point(3, 114);
-            this.btnJogar.Name = "btnJogar";
-            this.btnJogar.Size = new System.Drawing.Size(184, 42);
-            this.btnJogar.TabIndex = 2;
-            this.btnJogar.Text = "JOGAR";
-            this.btnJogar.UseVisualStyleBackColor = false;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DIGITE UMA LETRA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pnPalavra
             // 
@@ -105,6 +105,7 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "JOGO DA FORCA";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
--- prjC#/2J/TB/JogoDaForca/prjJogoDaForca/Form1.cs
+++ prjC#/2J/TB/JogoDaForca/prjJogoDaForca/Form1.cs
@@ -16,5 +16,25 @@ namespace prjJogoDaForca
         {
             InitializeComponent();
         }
+
+        List<string> lista = new List<string>()
+        {
+            "SANTOS","FREEFIRE","HEXALAB","DESIGNER","DIARIO","GAGO",
+            "MATHEUS","KAH","SAO PAULO","MAXIMOS","DEVELOPER","BACKEND",
+            "FRONTEND","COLEGIO","HERCULES","ANHANGUERA","AMAZONCRIPZ","COLMEIA",
+            "JOGADOR","MUNDIAL","RUSSIA","UCRANIA","COVID","BONDINHO","DATENA","PERCIVAL",
+            "BOLSOMINION","LULA"
+        };
+        Forca jogo;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            jogo  = new Forca(lista);
+            jogo.Sortear();
+            MessageBox.Show(jogo.DevolverPalavra());
+
+
+
+        }
     }
 }
