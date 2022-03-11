6a8fc23a5757e232c014ea531ad82caa4d483db6
Matheus Maximos Almeida <88410053+MatheusMaximosAlmeida@users.noreply.github.com>
2022-03-10 22:21:55 -0300

    add prjForca

---
+++ prjC#/2J/TB/JogoDaForca/prjJogoDaForca/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace prjJogoDaForca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLetra = new System.Windows.Forms.TextBox();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.pnPalavra = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnJogar);
+            this.panel1.Controls.Add(this.txtLetra);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(36, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(190, 174);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DIGITE UMA LETRA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtLetra
+            // 
+            this.txtLetra.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtLetra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLetra.Location = new System.Drawing.Point(3, 63);
+            this.txtLetra.MaxLength = 1;
+            this.txtLetra.Name = "txtLetra";
+            this.txtLetra.Size = new System.Drawing.Size(184, 31);
+            this.txtLetra.TabIndex = 1;
+            this.txtLetra.Text = "A";
+            this.txtLetra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnJogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJogar.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogar.ForeColor = System.Drawing.Color.White;
+            this.btnJogar.Location = new System.Drawing.Point(3, 114);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(184, 42);
+            this.btnJogar.TabIndex = 2;
+            this.btnJogar.Text = "JOGAR";
+            this.btnJogar.UseVisualStyleBackColor = false;
+            // 
+            // pnPalavra
+            // 
+            this.pnPalavra.Location = new System.Drawing.Point(251, 38);
+            this.pnPalavra.Name = "pnPalavra";
+            this.pnPalavra.Size = new System.Drawing.Size(573, 173);
+            this.pnPalavra.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 250);
+            this.Controls.Add(this.pnPalavra);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JOGO DA FORCA";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.TextBox txtLetra;
+        private System.Windows.Forms.Panel pnPalavra;
+    }
+}
+
